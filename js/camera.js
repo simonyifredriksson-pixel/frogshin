bda9af55a415eb3b54d23f563a130602b022363d
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-30 21:39:39 +0200

    v10: build-version handshake warns on mismatched clients

--- js/camera.js
+++ js/camera.js
@@ -6,9 +6,9 @@
  * screen shake used by every impactful action in the game.
  */
 
-import * as THREE from '../lib/three.module.js?v=v9';
-import { CFG } from './config.js?v=v9';
-import { clamp, damp, lerp } from './util.js?v=v9';
+import * as THREE from '../lib/three.module.js?v=v10';
+import { CFG } from './config.js?v=v10';
+import { clamp, damp, lerp } from './util.js?v=v10';
 
 const _desired = new THREE.Vector3();
 const _focus = new THREE.Vector3();
