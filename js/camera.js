4897deeb72b6189a5e297e8e58d9b247d5f74430
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-30 21:30:47 +0200

    Cache-bust all module URLs (v9) so players always get the newest build

--- js/camera.js
+++ js/camera.js
@@ -6,9 +6,9 @@
  * screen shake used by every impactful action in the game.
  */
 
-import * as THREE from '../lib/three.module.js';
-import { CFG } from './config.js';
-import { clamp, damp, lerp } from './util.js';
+import * as THREE from '../lib/three.module.js?v=v9';
+import { CFG } from './config.js?v=v9';
+import { clamp, damp, lerp } from './util.js?v=v9';
 
 const _desired = new THREE.Vector3();
 const _focus = new THREE.Vector3();
