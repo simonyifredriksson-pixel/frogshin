7ed32e2d790eafb53a3e098de524e27d4dd89746
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-30 21:39:44 +0200

    v10: build-version handshake warns on mismatched clients

--- js/effects.js
+++ js/effects.js
@@ -8,8 +8,8 @@
  * nothing is allocated during gameplay.
  */
 
-import * as THREE from '../lib/three.module.js?v=v9';
-import { clamp } from './util.js?v=v9';
+import * as THREE from '../lib/three.module.js?v=v10';
+import { clamp } from './util.js?v=v10';
 
 const VERT = `
   attribute float aSize;
