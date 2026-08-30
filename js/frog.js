320904cd163a1c6a227a2dffc93468897ed03328
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-30 21:58:42 +0200

    v12: clients now see the host in story; boss is strictly per-player

--- js/frog.js
+++ js/frog.js
@@ -8,8 +8,8 @@
  * every networked remote player.
  */
 
-import * as THREE from '../lib/three.module.js?v=v11';
-import { clamp, lerp, damp, dampAngle } from './util.js?v=v11';
+import * as THREE from '../lib/three.module.js?v=v12';
+import { clamp, lerp, damp, dampAngle } from './util.js?v=v12';
 
 const CLOTH = 0x24242e;        // ninja gi
 const CLOTH_DARK = 0x16161d;
