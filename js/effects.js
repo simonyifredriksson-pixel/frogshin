822ec76a9c5e220ebae193a5e52b6a731d82eae6
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-30 21:58:41 +0200

    v12: clients now see the host in story; boss is strictly per-player

--- js/effects.js
+++ js/effects.js
@@ -8,8 +8,8 @@
  * nothing is allocated during gameplay.
  */
 
-import * as THREE from '../lib/three.module.js?v=v11';
-import { clamp } from './util.js?v=v11';
+import * as THREE from '../lib/three.module.js?v=v12';
+import { clamp } from './util.js?v=v12';
 
 const VERT = `
   attribute float aSize;
