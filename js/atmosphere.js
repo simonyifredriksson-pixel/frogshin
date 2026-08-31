08d0a191313d8e827fd6643f58a20175514790fc
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:45:24 +0200

    v15: Froglets currency, pixel-art icons, parry in all modes, round clock ignores pause

--- js/atmosphere.js
+++ js/atmosphere.js
@@ -6,9 +6,9 @@
  * crisp contact shadows without the cost of shadowing the whole 420-unit map.
  */
 
-import * as THREE from '../lib/three.module.js?v=v14';
-import { CFG } from './config.js?v=v14';
-import { mulberry32 } from './util.js?v=v14';
+import * as THREE from '../lib/three.module.js?v=v15';
+import { CFG } from './config.js?v=v15';
+import { mulberry32 } from './util.js?v=v15';
 
 const SKY_VERT = `
   varying vec3 vWorld;
