f276931d698f955d699d24ee6a7ca45fb10456fd
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 21:08:56 +0200

    v17: team battle 1v1-5v5, practice ring try-out

--- js/net.js
+++ js/net.js
@@ -16,8 +16,8 @@
  * for one client to directly write another's health.
  */
 
-import { CFG, BUILD } from './config.js?v=v16';
-import { roomCode as makeRoomCode } from './util.js?v=v16';
+import { CFG, BUILD } from './config.js?v=v17';
+import { roomCode as makeRoomCode } from './util.js?v=v17';
 
 export const NetRole = { OFFLINE: 'offline', HOST: 'host', CLIENT: 'client' };
 
