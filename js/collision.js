5f1618772a40a85b3f714ac2ef01052d1344b916
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:00:34 +0200

    v13: add TURN relays so restrictive networks (school Wi-Fi) can connect

--- js/collision.js
+++ js/collision.js
@@ -8,8 +8,8 @@
  * several networked players are simulating at once.
  */
 
-import { CFG } from './config.js?v=v12';
-import { clamp } from './util.js?v=v12';
+import { CFG } from './config.js?v=v13';
+import { clamp } from './util.js?v=v13';
 
 const EPS = 1e-4;
 
