c8038016ae213d3f5db33a3645f61cffe63e1c28
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 21:08:46 +0200

    v17: team battle 1v1-5v5, practice ring try-out

--- js/economy.js
+++ js/economy.js
@@ -11,7 +11,7 @@
  * busy round, and localStorage is synchronous.
  */
 
-import { CFG } from './config.js?v=v16';
+import { CFG } from './config.js?v=v17';
 
 export class Economy {
   constructor() {
