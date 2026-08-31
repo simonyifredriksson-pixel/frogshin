f02d4964e0ea3c82f96ecc1afd44bd186b8facba
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 21:09:04 +0200

    v17: team battle 1v1-5v5, practice ring try-out

--- js/stamina.js
+++ js/stamina.js
@@ -8,8 +8,8 @@
  * without becoming a long dead stop.
  */
 
-import { CFG } from './config.js?v=v16';
-import { clamp } from './util.js?v=v16';
+import { CFG } from './config.js?v=v17';
+import { clamp } from './util.js?v=v17';
 
 export class Stamina {
   constructor(max = CFG.stamina.max) {
