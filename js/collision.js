c465491d76de20ce1928c7a62b92df909f6b84e1
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:45:26 +0200

    v15: Froglets currency, pixel-art icons, parry in all modes, round clock ignores pause

--- js/collision.js
+++ js/collision.js
@@ -8,8 +8,8 @@
  * several networked players are simulating at once.
  */
 
-import { CFG } from './config.js?v=v14';
-import { clamp } from './util.js?v=v14';
+import { CFG } from './config.js?v=v15';
+import { clamp } from './util.js?v=v15';
 
 const EPS = 1e-4;
 
