435b9da36c169b13916f78a0264d5afed5dbe499
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:57:21 +0200

    v16: shop, skin cases with CS-style opening, skins applied to models

--- js/economy.js
+++ js/economy.js
@@ -11,7 +11,7 @@
  * busy round, and localStorage is synchronous.
  */
 
-import { CFG } from './config.js?v=v15';
+import { CFG } from './config.js?v=v16';
 
 export class Economy {
   constructor() {
