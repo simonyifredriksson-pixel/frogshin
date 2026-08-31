6f5f24cdca66b2476dbc3eb8e8298186a023a727
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:45:32 +0200

    v15: Froglets currency, pixel-art icons, parry in all modes, round clock ignores pause

--- js/effects.js
+++ js/effects.js
@@ -8,8 +8,8 @@
  * nothing is allocated during gameplay.
  */
 
-import * as THREE from '../lib/three.module.js?v=v14';
-import { clamp } from './util.js?v=v14';
+import * as THREE from '../lib/three.module.js?v=v15';
+import { clamp } from './util.js?v=v15';
 
 const VERT = `
   attribute float aSize;
