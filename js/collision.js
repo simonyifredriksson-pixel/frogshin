93d1a16cc3328cb9d6c05330bd52bd396e62126a
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:11:17 +0200

    v14: fix stuck vote panel on leave; curving kunai aim assist

--- js/collision.js
+++ js/collision.js
@@ -8,8 +8,8 @@
  * several networked players are simulating at once.
  */
 
-import { CFG } from './config.js?v=v13';
-import { clamp } from './util.js?v=v13';
+import { CFG } from './config.js?v=v14';
+import { clamp } from './util.js?v=v14';
 
 const EPS = 1e-4;
 
