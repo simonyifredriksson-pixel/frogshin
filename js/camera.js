ab7d9f490f99534798e30964545b1fc66727f973
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:00:33 +0200

    v13: add TURN relays so restrictive networks (school Wi-Fi) can connect

--- js/camera.js
+++ js/camera.js
@@ -6,9 +6,9 @@
  * screen shake used by every impactful action in the game.
  */
 
-import * as THREE from '../lib/three.module.js?v=v12';
-import { CFG } from './config.js?v=v12';
-import { clamp, damp, lerp } from './util.js?v=v12';
+import * as THREE from '../lib/three.module.js?v=v13';
+import { CFG } from './config.js?v=v13';
+import { clamp, damp, lerp } from './util.js?v=v13';
 
 const _desired = new THREE.Vector3();
 const _focus = new THREE.Vector3();
