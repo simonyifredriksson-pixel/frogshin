4d591fc9659468b7958782d5650ec29f430b388d
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:45:35 +0200

    v15: Froglets currency, pixel-art icons, parry in all modes, round clock ignores pause

--- js/grapple.js
+++ js/grapple.js
@@ -11,9 +11,9 @@
  *      aggressive rather than like a slow winch.
  */
 
-import * as THREE from '../lib/three.module.js?v=v14';
-import { CFG } from './config.js?v=v14';
-import { clamp } from './util.js?v=v14';
+import * as THREE from '../lib/three.module.js?v=v15';
+import { CFG } from './config.js?v=v15';
+import { clamp } from './util.js?v=v15';
 
 export const GrappleState = {
   IDLE: 0,
