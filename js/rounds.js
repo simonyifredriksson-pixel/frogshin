a917bb9a8518a13845e8d26e104369feada40895
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:11:35 +0200

    v14: fix stuck vote panel on leave; curving kunai aim assist

--- js/rounds.js
+++ js/rounds.js
@@ -17,8 +17,8 @@
  *               everyone is infected, or the survivors run out the clock.
  */
 
-import { CFG } from './config.js?v=v13';
-import { clamp } from './util.js?v=v13';
+import { CFG } from './config.js?v=v14';
+import { clamp } from './util.js?v=v14';
 
 export const MODES = { TAG: 'tag', INFECTION: 'infection', FFA: 'ffa' };
 
