6d090fc1d5826fe6d605850a78fa610aed7b0acf
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:11:28 +0200

    v14: fix stuck vote panel on leave; curving kunai aim assist

--- js/menu.js
+++ js/menu.js
@@ -8,11 +8,11 @@
  * full world generation.
  */
 
-import * as THREE from '../lib/three.module.js?v=v13';
-import { ValueNoise, mulberry32, clamp, lerp, smoothstep } from './util.js?v=v13';
-import { Atmosphere } from './atmosphere.js?v=v13';
-import { FrogModel } from './frog.js?v=v13';
-import { lanternGlowTexture } from './world.js?v=v13';
+import * as THREE from '../lib/three.module.js?v=v14';
+import { ValueNoise, mulberry32, clamp, lerp, smoothstep } from './util.js?v=v14';
+import { Atmosphere } from './atmosphere.js?v=v14';
+import { FrogModel } from './frog.js?v=v14';
+import { lanternGlowTexture } from './world.js?v=v14';
 
 const _m = new THREE.Matrix4();
 const _q = new THREE.Quaternion();
