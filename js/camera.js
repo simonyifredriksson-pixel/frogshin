f69fcb9520f491f9fd561891f251c2f928a0e5dd
simonyifredriksson-pixel <simon.yifredriksson@gmail.com>
2026-08-31 20:45:25 +0200

    v15: Froglets currency, pixel-art icons, parry in all modes, round clock ignores pause

--- js/camera.js
+++ js/camera.js
@@ -6,9 +6,9 @@
  * screen shake used by every impactful action in the game.
  */
 
-import * as THREE from '../lib/three.module.js?v=v14';
-import { CFG } from './config.js?v=v14';
-import { clamp, damp, lerp } from './util.js?v=v14';
+import * as THREE from '../lib/three.module.js?v=v15';
+import { CFG } from './config.js?v=v15';
+import { clamp, damp, lerp } from './util.js?v=v15';
 
 const _desired = new THREE.Vector3();
 const _focus = new THREE.Vector3();
